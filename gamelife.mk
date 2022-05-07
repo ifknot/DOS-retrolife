8a5b6c2e5c270d203875bf2d3d478128c7fa7b00
ifknot <ifknot@hotmail.com>
2022-05-08 00:40:03 +0100

    VS project

---
+++ gamelife.mk
@@ -0,0 +1,3 @@
+project : C:\Users\ifkno\DOSBOX\DOS16\GAMELIFE\gamelife.exe .SYMBOLIC
+
+!include C:\Users\ifkno\DOSBOX\DOS16\GAMELIFE\gamelife.mk1
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Conway's Game of Life\n";
+  std::cout << "Conway's Game of Life...\n";
 }
