493a5c490f3aa01fd84a6d8fe25050c47dd71f63
ifknot <ifknot@hotmail.com>
2022-05-09 20:03:28 +0100

    plot pixel

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
 
-#include "automata/cell_space.h"
+#include <iostream>
+#include <stdint.h>
+#include <vector>
+
+#include "retrolib/gfx/hga/test_hga.h"
 
 int main() {
   std::cout << "Conway's Game of Life...\n";
 
+  
+  test_hga::run();
   
 
 }
--- retrolib/gfx/hga/hga_plot_point.h
+++ retrolib/gfx/hga/hga_plot_point.h
@@ -46,115 +46,142 @@
 
 namespace hga {
 
-        namespace screen_bound {
+    struct point_t {
 
-                /**
-                *  @brief plot to default display page 0, clipping to fit within the screen bounds
-                *  @param x      - screen x coordinate, (0,0) top-left
-                *  @param y      - screen y coordinate, (719,347) bottom right
-                *  @param colour - background 0 foreground 1
-                */
-                void plot_point(size_type x, size_type y, colour_t colour) {
-                        __asm {
-                            .8086
+        uint16_t x, y;
 
-                            mov     ax, HGA_VIDEO_RAM_SEGMENT
-                            mov     es, ax
+    };
 
-                            mov     ax, y           ; load y into bx then perform screen clipping
-                            mov     dx, ax          ; copy y
-                            cmp     ax, SCREEN_Y_MAX ; compare bx with y maximum boundry
-                            jge     END             ; nothing to plot
-                            
+    union union_point_t {
 
-#ifdef ENABLE_MUL           
-                            shr     ax, 1       ; calculate y / 4
-                            shr     ax, 1       ; 8086 limited to single step shifts
-                            mov     cl, BYTES_PER_LINE
-                            mul     cl          ; calculate(y / 4) * 90
-#else
-                            // bit shifting * 90
-                            and     ax, 0FFFCh  ; mask out bank selection bits
-                            mov     bx, ax      ; copy y                   
-                            //shr     ax, 1       ; calculate y / 4
-                            //shr     ax, 1       ; 8086 limited to single step shifts
-                            
-                            // (y / 4) * 80
-                            // simplifies to y * 20
-                            //shl     ax, 1       ; 8086 shift left 4 times
-                            //shl     ax, 1       ; y * 16
-                            shl     ax, 1
-                            shl     ax, 1
-                            mov     cx, ax      ; copy y 
-                            shl     ax, 1       ; 8086 shift left 2 times
-                            shl     ax, 1       ; 
-                            add     ax, cx      ; 
-                            // (y / 4) * 10
-                            // simplifies to (y / 2) * 4
-                            //shr     bx, 1       ; calculate y / 4
-                            shr     bx, 1       ; 8086 limited to single step shifts
-                            //shl     bx, 1       
-                            mov     cx, bx      ; copy y 
-                            shl     bx, 1       ; 8086 shift left 2 times
-                            shl     bx, 1       ; 
-                            add     ax, cx      ; + (y / 2)
-                            add     ax, bx      ; + (y * 4)
-#endif                      
-                            and     dx, 00000011b           ; mask off only the bank select bits (0..4)
-                            ror     dx, 1                   ; calculate 16-bit bank# * 2000h
-                            ror     dx, 1                   ; 8086 limited to single step shifts
-                            ror     dx, 1                   ; (y mod 4) * 2000h
+        uint32_t dword;
+        point_t words;
 
-                            mov     di, x                   ; load x into di and clip to screen bounds
-                            cmp     di, SCREEN_X_MAX        ; compare di with x maximum boundry
-                            jge     END                     ; nothing to plot
+        union_point_t() : dword(0) {}
 
-                            mov     cx, di                  ; copy of x in cx
-                                
-                            shr     di, 1                   ; calculate column byte x / 8
-                            shr     di, 1                   ; 8086 limited to single step shifts
-                            shr     di, 1                   ; x / 8
+        union_point_t(uint32_t dword) : dword(dword) {}
 
-                            add     di, ax                  ; + (y / 4) * 90
-                            add     di, dx                  ; + (y mod 4) * 2000h
-
-                            and     cx, 7h                  ; mask off 0111 lower bits i.e.mod 8 (thanks powers of 2)
-                            mov     al, 01111111b           ; load al with pixel mask
-                            mov     ah, colour              ; load ah with a single pixel at msb (e.g. white 10000000)
-                            ror		al, cl			        ; roll mask around by x mod 8
-                            shr     ah, cl                  ; shift single bit along by x mod 8
-
-#ifdef SYNCHRONISED	// will need al so cost an extra mov reg, reg to copy into cl
-					        mov		cl, al			; copy mask byte
-					        mov     dx, CGA_STATUS_REG  ; CGA status reg
-				        L0: in      al, dx          ; read status
-					        test    al, 1000b       ; is bit 3 set ? (in a vertical retrace interval)
-					        jnz     L0              ; yes, keep waiting
-				        L1: in      al, dx          ; read status
-					        test    al, 1000b       ; is bit 3 set ? (just started a vertical retrace interval)
-					        jnz     L1              ; no, keep waiting
-
-					        and		es:[di], cl		; mask out the pixel bits
-#else
-					        and		es:[di], al		; mask out the pixel bits
-#endif					
-					        or		es:[di], ah		; plot point
-                               
-                END:
-                        }
-                }
-
-                /**
-                *  @brief plot to selected display page, clipping to fit within the screen bounds
-                *  @param x      - x coordinate, where (0,0) top-left and (639,199) bottom-right
-                *  @param y      -
-                *  @param colour - background 0 foreground 1
-                *  @param page   - display page 0 or 1
-                */
-                void plot_point(size_type x, size_type y, colour_t colour, uint8_t page) {
-                }
-
+        union_point_t(uint16_t x, uint16_t y) {
+            words.x = x;
+            words.y = y;
         }
+
+    };
+
+    namespace screen_bound {
+
+        /**
+        *  @brief plot to default display page 0, clipping to fit within the screen bounds
+        *  @param x      - screen x coordinate, (0,0) top-left
+        *  @param y      - screen y coordinate, (719,347) bottom right
+        *  @param colour - background 0 foreground 1
+        */
+        inline void plot_point(size_type x, size_type y, colour_t colour) {
+                __asm {
+                    .8086
+
+                    mov     ax, HGA_VIDEO_RAM_SEGMENT
+                    mov     es, ax
+
+                    mov     ax, y           ; load y into bx then perform screen clipping
+                    mov     dx, ax          ; copy y
+                    cmp     ax, SCREEN_Y_MAX ; compare bx with y maximum boundry
+                    jge     END             ; nothing to plot
+
+
+#ifdef ENABLE_MUL
+                    shr     ax, 1       ; calculate y / 4
+                    shr     ax, 1       ; 8086 limited to single step shifts
+                    mov     cl, BYTES_PER_LINE
+                    mul     cl          ; calculate(y / 4) * 90
+#else
+                    // bit shifting * 90
+                    and     ax, 0FFFCh  ; mask out bank selection bits
+                    mov     bx, ax      ; copy y
+                    //shr     ax, 1       ; calculate y / 4
+                    //shr     ax, 1       ; 8086 limited to single step shifts
+
+                    // (y / 4) * 80
+                    // simplifies to y * 20
+                    //shl     ax, 1       ; 8086 shift left 4 times
+                    //shl     ax, 1       ; y * 16
+                    shl     ax, 1
+                    shl     ax, 1
+                    mov     cx, ax      ; copy y
+                    shl     ax, 1       ; 8086 shift left 2 times
+                    shl     ax, 1       ;
+                    add     ax, cx      ;
+                    // (y / 4) * 10
+                    // simplifies to (y / 2) * 4
+                    //shr     bx, 1       ; calculate y / 4
+                    shr     bx, 1       ; 8086 limited to single step shifts
+                    //shl     bx, 1
+                    mov     cx, bx      ; copy y
+                    shl     bx, 1       ; 8086 shift left 2 times
+                    shl     bx, 1       ;
+                    add     ax, cx      ; + (y / 2)
+                    add     ax, bx      ; + (y * 4)
+#endif
+                    and     dx, 00000011b           ; mask off only the bank select bits (0..4)
+                    ror     dx, 1                   ; calculate 16-bit bank# * 2000h
+                    ror     dx, 1                   ; 8086 limited to single step shifts
+                    ror     dx, 1                   ; (y mod 4) * 2000h
+
+                    mov     di, x                   ; load x into di and clip to screen bounds
+                    cmp     di, SCREEN_X_MAX        ; compare di with x maximum boundry
+                    jge     END                     ; nothing to plot
+
+                    mov     cx, di                  ; copy of x in cx
+
+                    shr     di, 1                   ; calculate column byte x / 8
+                    shr     di, 1                   ; 8086 limited to single step shifts
+                    shr     di, 1                   ; x / 8
+
+                    add     di, ax                  ; + (y / 4) * 90
+                    add     di, dx                  ; + (y mod 4) * 2000h
+
+                    and     cx, 7h                  ; mask off 0111 lower bits i.e.mod 8 (thanks powers of 2)
+                    mov     al, 01111111b           ; load al with pixel mask
+                    mov     ah, colour              ; load ah with a single pixel at msb (e.g. white 10000000)
+                    ror         al, cl                          ; roll mask around by x mod 8
+                    shr     ah, cl                  ; shift single bit along by x mod 8
+
+#ifdef SYNCHRONISED     // will need al so cost an extra mov reg, reg to copy into cl
+                                        mov             cl, al                  ; copy mask byte
+                                        mov     dx, CGA_STATUS_REG  ; CGA status reg
+                                L0: in      al, dx          ; read status
+                                        test    al, 1000b       ; is bit 3 set ? (in a vertical retrace interval)
+                                        jnz     L0              ; yes, keep waiting
+                                L1: in      al, dx          ; read status
+                                        test    al, 1000b       ; is bit 3 set ? (just started a vertical retrace interval)
+                                        jnz     L1              ; no, keep waiting
+
+                                        and             es:[di], cl             ; mask out the pixel bits
+#else
+                                        and             es:[di], al             ; mask out the pixel bits
+#endif
+                                        or              es:[di], ah             ; plot point
+
+        END:
+                }
+        }
+
+        /**
+        *  @brief plot to selected display page, clipping to fit within the screen bounds
+        *  @param x      - x coordinate, where (0,0) top-left and (639,199) bottom-right
+        *  @param y      -
+        *  @param colour - background 0 foreground 1
+        *  @param page   - display page 0 or 1
+        */
+        void plot_point(size_type x, size_type y, colour_t colour, uint8_t page) {
+        }
+
+
+        plot_point(union_point_t point) {
+            std::cout << point.words.x << ',' << point.words.y << '\n';
+        }
+
+    }
 
 }
 
