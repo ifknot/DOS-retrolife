3586636e44ce0ca650e03b5c689a82bbc733de4a
ifknot <ifknot@hotmail.com>
2022-05-19 11:22:13 +0100

    update seg mem block into this retrolib

--- main.cpp
+++ main.cpp
@@ -10,29 +10,7 @@
 int main() {
   std::cout << "Conway's Game of Life...\n";
 
-  dos::segmented_memory_block<uint8_t> cells8(16);
-  cells8.fill('A');
-  cells8.dump();
-  std::cout << "capacity " << cells8.capacity() << '\n'
-	  << "bytes " << cells8.bytes() << '\n'
-	  << "paragraphs " << cells8.paragraphs() << '\n';
-
-  dos::segmented_memory_block<uint16_t> cells16(16);
-  cells16.fill(0x4142);
-  cells16.dump();
-  std::cout << std::hex << cells16[15] << '\n';
-  std::cout << "capacity " << cells16.capacity() << '\n'
-	  << "bytes " << cells16.bytes() << '\n'
-	  << "paragraphs " << cells16.paragraphs() << '\n';
-
-  dos::segmented_memory_block<uint32_t> cells32(16);
-  cells32.fill(0x41424344);
-  cells32[15] = 0x44434241;
-  cells32.dump();
-  std::cout << std::hex << cells32[15] << '\n';
-  std::cout << "capacity " << cells32.capacity() << '\n'
-	  << "bytes " << cells32.bytes() << '\n'
-	  << "paragraphs " << cells32.paragraphs() << '\n';
+  
   
 
   //test_hga::run();
--- retrolib/dos/test_memory_widgets.h
+++ retrolib/dos/test_memory_widgets.h
@@ -31,6 +31,30 @@ namespace test_memory_widgets {
 			for (int i = 0; i < m.size; ++i) { assert(m[i] == 'B'); }
 			m[8] = 'A';
 			assert(m[8] == 'A');
+
+			dos::segmented_memory_block<uint8_t> cells8(16);
+			cells8.fill('A');
+			cells8.dump();
+			std::cout << "capacity " << cells8.capacity() << '\n'
+				<< "bytes " << cells8.bytes() << '\n'
+				<< "paragraphs " << cells8.paragraphs() << '\n';
+
+			dos::segmented_memory_block<uint16_t> cells16(16);
+			cells16.fill(0x4142);
+			cells16.dump();
+			std::cout << std::hex << cells16[15] << '\n';
+			std::cout << "capacity " << cells16.capacity() << '\n'
+				<< "bytes " << cells16.bytes() << '\n'
+				<< "paragraphs " << cells16.paragraphs() << '\n';
+
+			dos::segmented_memory_block<uint32_t> cells32(16);
+			cells32.fill(0x41424344);
+			cells32[15] = 0x44434241;
+			cells32.dump();
+			std::cout << std::hex << cells32[15] << '\n';
+			std::cout << "capacity " << cells32.capacity() << '\n'
+				<< "bytes " << cells32.bytes() << '\n'
+				<< "paragraphs " << cells32.paragraphs() << '\n';
 		}
 		std::cout << "success!\n";
 	}
