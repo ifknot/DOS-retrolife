54fe323e464b655b2f36e31eb62032131b34a6d6
ifknot <ifknot@hotmail.com>
2022-05-23 18:13:28 +0100

    writer

---
+++ retrolib/file/writer.h
@@ -0,0 +1,28 @@
+/**
+ *
+ *  @brief     
+ *  @details   ~
+ *  @author    Jeremy Thornton
+ *  @date      23.05.2022
+ *  @copyright © Jeremy Thornton, 2022. All right reserved.
+ *
+ */
+#ifndef WRITER
+#define WRITER
+
+/*
+
+Writer	append(char c)	It appends the specified character to this writer.
+Writer	append(CharSequence csq)	It appends the specified character sequence to this writer
+Writer	append(CharSequence csq, int start, int end)	It appends a subsequence of the specified character sequence to this writer.
+abstract void	close()	It closes the stream, flushing it first.
+abstract void	flush()	It flushes the stream.
+void	write(char[] cbuf)	It writes an array of characters.
+abstract void	write(char[] cbuf, int off, int len)	It writes a portion of an array of characters.
+void	write(int c)	It writes a single character.
+void	write(String str)	It writes a string.
+void	write(String str, int off, int len)	It writes a portion of a string.
+
+*/
+
+#endif
