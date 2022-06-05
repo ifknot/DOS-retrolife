dcec07d2378c73f558dcc7b6869c27bf85a06e36
ifknot <ifknot@hotmail.com>
2022-06-05 20:00:33 +0100

    test_count_neighbours

---
+++ retrolib/automata/test_count_neighbours.h
@@ -0,0 +1,28 @@
+/**
+ *
+ *  @brief     
+ *  @details   ~
+ *  @author    Jeremy Thornton
+ *  @date      5.06.2022
+ *  @copyright © Jeremy Thornton, 2022. All right reserved.
+ *
+ */
+#ifndef TEST_COUNT_NEIGHBOURS_H
+#define TEST_COUNT_NEIGHBOURS_H
+
+#include <cassert>
+#include <iostream>
+
+namespace test_count_neighbours {
+
+	void run() {
+		std::cout << "test_count_neighbours...\n";
+		{
+		
+		}
+		std::cout << "success!\n";
+	}
+
+}
+
+#endif
--- retrolib/gfx/point_vector_2d.h
+++ retrolib/gfx/point_vector_2d.h
@@ -29,15 +29,15 @@ namespace jtl{
         template<size_t N>
         class point_vector_2d {
 
-                typedef data_vector<uint32_t, N> point_vector_t;
+            typedef data_vector<uint32_t, N> point_vector_t;
 
         public:
 
 // member types
 
-                typedef uint32_t point_type;
-                typedef const point_type* const_pointer;
-                typedef size_t size_type;
+            typedef uint32_t point_type;
+            typedef const point_type* const_pointer;
+            typedef size_t size_type;
 
 // construction
 
@@ -45,26 +45,26 @@ namespace jtl{
 
 // capacity
 
-                inline size_type size() const {
-                    return points.size();
-                }
+            inline size_type size() const {
+                return points.size();
+            }
 
 // element access
 
-                inline const_pointer data() const {
-                    return points.data();
-                }
+            inline const_pointer data() const {
+                return points.data();
+            }
 
 // modifiers
 
-                void add(size_type x, size_type y) {
-                    union_point_t point(x, y);
-                    points.push_back(point.dword);
-                }
+            void add(size_type x, size_type y) {
+                union_point_t point(x, y);
+                points.push_back(point.dword);
+            }
 
-                inline void clear() {
-                    points.clear();
-                }
+            inline void clear() {
+                points.clear();
+            }
 
 // relational operators
 
@@ -72,78 +72,77 @@ namespace jtl{
 
 //manipulating operations
 
-                void translate(size_type x, size_type y) {
-                    union_point_t point(x, y);
-                    for (point_vector_t::iterator it = points.begin(); it < points.end(); ++it) {
-                            *it += point.dword;
-                    }
+            void translate(size_type x, size_type y) {
+                union_point_t point(x, y);
+                for (point_vector_t::iterator it = points.begin(); it < points.end(); ++it) {
+                        *it += point.dword;
                 }
+            }
 
 // serialisation
 
-        std::istream& read(std::istream& is) {
-            return points.read(is);
-        }
+            std::istream& read(std::istream& is) {
+                return points.read(is);
+            }
 
-        std::ostream& write(std::ostream& os) const {
-            for (size_t i = 0; i < points.size(); ++i) {
-                union_point_t point(points[i]);
-                os << point.p.x << ' ' << point.p.y << ' ';
+            std::ostream& write(std::ostream& os) const {
+                for (size_t i = 0; i < points.size(); ++i) {
+                    union_point_t point(points[i]);
+                    os << point.p.x << ' ' << point.p.y << ' ';
+                }
+                return os;
             }
-            return os;
-        }
 
-        void read(std::string file_path) {
-            std::ifstream is(file_path.c_str());
-            if (!is.is_open()) {
-                std::cerr << dos::error::messages[dos::error::FILE_NOT_FOUND] << file_path.c_str() << '\n';
+            void read(std::string file_path) {
+                std::ifstream is(file_path.c_str());
+                if (!is.is_open()) {
+                    std::cerr << dos::error::messages[dos::error::FILE_NOT_FOUND] << file_path.c_str() << '\n';
+                }
+                point_vector_t::size_type size;
+                is >> size;
+                points.resize(size);
+                for (size_t i = 0; i < points.size(); ++i) {
+                    union_point_t point;
+                    is >> point.p.x;
+                    is >> point.p.y;
+                    points[i] = point.dword;
+                }
             }
-            point_vector_t::size_type size;
-            is >> size;
-            points.resize(size);
-            for (size_t i = 0; i < points.size(); ++i) {
-                union_point_t point;
-                is >> point.p.x;
-                is >> point.p.y;
-                points[i] = point.dword;
-            }
-        }
 
-        void write(std::string file_path) {
-            std::ofstream os(file_path.c_str());
-            if (!os.is_open()) {
-                std::cerr << dos::error::messages[dos::error::FILE_NOT_FOUND] << file_path.c_str() << '\n';
+            void write(std::string file_path) {
+                std::ofstream os(file_path.c_str());
+                if (!os.is_open()) {
+                    std::cerr << dos::error::messages[dos::error::FILE_NOT_FOUND] << file_path.c_str() << '\n';
+                }
+                os << size() << ' ';
+                write(os);
             }
-            os << size() << ' ';
-            write(os);
-        }
 
-        void read_binary(std::string file_path) {
-            std::ifstream is(file_path.c_str());
-            if (!is.is_open()) {
-                std::cerr << dos::error::messages[dos::error::FILE_NOT_FOUND] << file_path.c_str() << '\n';
+            void read_binary(std::string file_path) {
+                std::ifstream is(file_path.c_str());
+                if (!is.is_open()) {
+                    std::cerr << dos::error::messages[dos::error::FILE_NOT_FOUND] << file_path.c_str() << '\n';
+                }
+                union_dword_t header;
+                is.read(reinterpret_cast<char*>(&header.word), sizeof(header.word));
+                points.resize(header.word.hi);
+                is.read(reinterpret_cast<char*>(points.data()), size() * sizeof(point_vector_t::value_type));
             }
-            union_dword_t header;
-            is.read(reinterpret_cast<char*>(&header.word), sizeof(header.word));
-            points.resize(header.word.hi);
-            is.read(reinterpret_cast<char*>(points.data()), size() * sizeof(point_vector_t::value_type));
-        }
 
-        void write_binary(std::string file_path) {
-            std::ofstream os(file_path.c_str());
-            if (!os.is_open()) {
-                std::cerr << dos::error::messages[dos::error::FILE_NOT_FOUND] << file_path.c_str() << '\n';
+            void write_binary(std::string file_path) {
+                std::ofstream os(file_path.c_str());
+                if (!os.is_open()) {
+                    std::cerr << dos::error::messages[dos::error::FILE_NOT_FOUND] << file_path.c_str() << '\n';
+                }
+                uint16_t sum = checksum(points.data(), size());
+                union_dword_t header(size(), sum);
+                os.write(reinterpret_cast<const char*>(&header.dword), sizeof(header.dword));
+                os.write(reinterpret_cast<const char*>(points.data()), size() * sizeof(point_vector_t::value_type));
             }
-            uint16_t sum = checksum(points.data(), size());
-            union_dword_t header(size(), sum);
-            os.write(reinterpret_cast<const char*>(&header.dword), sizeof(header.dword));
-            os.write(reinterpret_cast<const char*>(points.data()), size() * sizeof(point_vector_t::value_type));
-        }
 
         private:
 
                 point_vector_t points;
-
 
         };
 
