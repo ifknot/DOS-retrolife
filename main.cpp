3f418fe9b1244325009b584154183c4d52afd1fd
ifknot <ifknot@hotmail.com>
2022-06-06 17:04:50 +0100

    hga_count_plot_multi_point

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@
 #include <vector>
 
 //#include "retrolib/dos/dos_segmented_memory_block.h"
-#include "retrolib/gfx/hga/test_hga.h"
+//#include "retrolib/gfx/hga/test_hga.h"
 //#include "retrolib/file/test_file_utilities.h"
 //#include "retrolib/file/test_file_streams.h"
 //#include "retrolib/memory/test_array.h"
 //#include "retrolib/memory/test_data_vector.h"
 //#include "retrolib/gfx/test_point_vector_2d.h"
 //#include "retrolib/memory/test_memory_unions.h"
-//#include "retrolib/automata/test_count_neighbours.h"
+#include "retrolib/automata/test_count_neighbours.h"
 
 int main() {
 
-    test_hga::run();
-    //test_count_neighbours::run();
-    //test_point_vector_2d::run();
+    //test_hga::run();
+    test_count_neighbours::run();
+    
 }
---
+++ retrolib/gfx/hga/hga_count_plot_multi_point.h
@@ -0,0 +1,27 @@
+/**
+ *
+ *  @brief     
+ *  @details   ~
+ *  @author    Jeremy Thornton
+ *  @date      6.06.2022
+ *  @copyright © Jeremy Thornton, 2022. All right reserved.
+ *
+ */
+#ifndef HGA_COUNT_PLOT_MULTIPOINT_H
+#define HGA_COUNT_PLOT_MULTIPOINT_H
+
+#include "hga_constants.h"
+
+namespace hga {
+
+	namespace screen_bound {
+
+		size_type hga_count_plot_multi_point(const uint32_t* point_data, uint16_t size, uint8_t buffer = 0) {
+
+		}
+
+	}
+
+}
+
+#endif
