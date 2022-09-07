d95adadeb5cc78b1e2b9ffe993048d3845092e26
ifknot <ifknot@hotmail.com>
2022-09-07 14:55:59 +0100

    Create draft_write_bitmap.h

---
+++ draft_write_bitmap.h
@@ -0,0 +1,10 @@
+
+// DS:SI points to bitmap - 
+// word size
+// word offset in bytes to pixel data 
+// word width
+// word height
+// byte depth
+// byte mode - DOS screen modes + HGA0, HGA1, HGA2 (for herc, herc+, herc incolor)
+// ...
+// raw pixel data
